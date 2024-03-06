e7c238427351658411ccd06a3e2fc89d5de6d875
diegohat <diego.hat7@gmail.com>
2024-03-06 18:16:36 -0300

    add select_random

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::io;
+
+use rand::{thread_rng, Rng};
+
+
+fn select_random_word(fruits: &[&str]) -> String {
+    
+    let mut rng = thread_rng();
+    let index = rng.gen_range(0..fruits.len());
+    String::from(fruits[index])
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let fruits = ["apple", "banana", "orange", "pear", "grape", "pineapple", "watermelon", "strawberry", "blueberry", "kiwi"];
+
+    println!("Welcome to Guessing Game! Press ENTER to start...");
+    let mut input = String::new();
+    io::stdin()
+    .read_line(&mut input)
+    .expect("Failed to start game.");
+    // TODO - add a select topic option
+    println!("Generating a random word...");
+    let random_word = select_random_word(&fruits);
+
+    println!("Enter a fruit name: ");
+    io::stdin()
+    .read_line(&mut input)
+    .expect("Failed to read word.");
 }

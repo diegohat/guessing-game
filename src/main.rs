e79ae3ce47c3e1e86595a8bd61af115eda4b849b
diegohat <diego.hat7@gmail.com>
2024-03-07 08:52:08 -0300

    finish game

--- src/main.rs
+++ src/main.rs
@@ -10,21 +10,44 @@ fn select_random_word(fruits: &[&str]) -> String {
     String::from(fruits[index])
 }
 
+fn check_word(guess: &str, random_word: &str) -> bool {
+    let trimmed_guess = guess.trim();
+    // println!("Guessed: '{}'", trimmed_guess);
+    // println!("Random: '{}'", random_word);
+    if trimmed_guess == random_word {
+        println!("You guessed the word!");
+        false
+    } else {
+        println!("Wrong!");
+        true
+    }
+}
 
 fn main() {
     let fruits = ["apple", "banana", "orange", "pear", "grape", "pineapple", "watermelon", "strawberry", "blueberry", "kiwi"];
 
     println!("Welcome to Guessing Game! Press ENTER to start...");
-    let mut input = String::new();
+    let mut guess = String::new();
     io::stdin()
-    .read_line(&mut input)
-    .expect("Failed to start game.");
-    // TODO - add a select topic option
+        .read_line(&mut guess)
+        .expect("Failed to start game.");
+
     println!("Generating a random word...");
     let random_word = select_random_word(&fruits);
 
-    println!("Enter a fruit name: ");
-    io::stdin()
-    .read_line(&mut input)
-    .expect("Failed to read word.");
+    loop {
+        println!("Enter a fruit name: ");
+        guess.clear();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read word.");
+
+        if !check_word(&guess, &random_word) {
+            break;
+        } else {
+            println!("Try again!");
+        }
+    }
+
+    println!("Good game!!");
 }
